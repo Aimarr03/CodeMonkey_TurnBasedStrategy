d682a739449bbfa2afc60488951702645b6c04bf
Aimarr03 <andyaimar729@gmail.com>
2023-11-08 06:12:07 +0700

    Phase 01 Testing Animation based on scripting

--- CodeMonkey_TurnBasedStrategy/Assets/Scripts/Unit/Unit.cs
+++ CodeMonkey_TurnBasedStrategy/Assets/Scripts/Unit/Unit.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
     private Vector3 targetPosition;
+    const string IsWalking = "IsWalking";
     private float speed;
     private void Start() {
         targetPosition = transform.position;
-        speed = 4f;
+        speed = 2f;
     }
     private void Update() {
         if(Vector3.Distance(targetPosition, transform.position) > 0.3f){
-            Vector3 MoveDirection = (targetPosition - transform.position);
+            Vector3 MoveDirection = targetPosition - transform.position;
             transform.position += MoveDirection * speed * Time.deltaTime;
+            animator.SetBool("IsWalking", true);
+        }
+        else{
+            animator.SetBool("IsWalking", false);
         }
         if(Input.GetMouseButton(0)){
             Move(MouseInput.GetMousePosition());
+            
         }
     }
 
